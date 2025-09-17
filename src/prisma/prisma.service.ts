45207d2386ec6b2f89be673a8748a7d5e3fdad4f
matheusjean <matheusjean11@hotmail.com>
2025-09-16 22:38:47 -0300

    feat: add Prisma schema and service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,29 @@
+import {
+  INestApplication,
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    app.enableShutdownHooks();
+
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
