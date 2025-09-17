926003dd62a70d2f66a9ba9214518b4f660b5c4b
matheusjean <matheusjean11@hotmail.com>
2025-09-17 13:48:51 -0300

    chore: update courses module

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -4,6 +4,7 @@ import { CoursesController } from './courses.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-redis-store';
+import { MetricsModule } from 'src/metrics/metrics.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import * as redisStore from 'cache-manager-redis-store';
       port: parseInt(process.env.REDIS_PORT ?? '6379', 10),
       ttl: 30,
     }),
+    MetricsModule,
   ],
   controllers: [CoursesController],
   providers: [CoursesService, PrismaService],
